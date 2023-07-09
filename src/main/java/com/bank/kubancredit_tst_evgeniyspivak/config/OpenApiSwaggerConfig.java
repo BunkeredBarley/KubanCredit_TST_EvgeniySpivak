90f801b520cd4b92a8dfaa88f5c10881c62644b2
Evgeniy S <bunkeredbarley@gmail.com>
2023-07-09 21:38:03 +0300

    Add Swagger

---
+++ src/main/java/com/bank/kubancredit_tst_evgeniyspivak/config/OpenApiSwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.bank.kubancredit_tst_evgeniyspivak.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Tasks Api", version = "1.0.0.1",
+        contact = @Contact(name = "Evgeniy Spivak")))
+public class OpenApiSwaggerConfig {
+
+}
